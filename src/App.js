ef513c34b577d3943bbf2351df0228ed13b8f690
malapashish <malapashish10@gmail.com>
2021-04-30 13:43:13 +0530

    Version 1 Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
+import React,  { useState , useEffect } from 'react';
+import firebase from 'firebase/app';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import EditorComponent from './editor/editor';
+import SidebarComponent from './sidebar/sidebar';  
+
+const App = () =>{
+    const [ selectedNoteIndex , setSelectedNoteIndex ] = useState(null);
+    const [ selectedNote , setSelectedNote ] = useState(null);
+    const [ notes , setNotes ] = useState(null); 
+
+    useEffect(() => {
+        firebase
+        .firestore()
+        .collection('notes')
+        .onSnapshot(serverUpdate => {
+            const notes = serverUpdate.docs.map(_doc => {
+                const data = _doc.data();
+                data['id'] = _doc.id;
+                return data;
+            });
+            setNotes(notes);
+        }) 
+    } , [])
+
+    const selectNote = (note , index) => {
+        setSelectedNote(note);
+        setSelectedNoteIndex(index);
+    }
+
+    const noteUpdate = (id , noteObject) => {
+        firebase
+                .firestore()
+                .collection('notes')
+                .doc(id)
+                .update({
+                    title : noteObject.title,
+                    body : noteObject.body , 
+                    timestamp : firebase.firestore.FieldValue.serverTimestamp()
+                });
+    }
+
+    const newNote = async (title) => {
+        const note = {
+            title : title ,
+            body : ''
+        };
+
+        const newFromDB = await firebase
+                                        .firestore()
+                                        .collection('notes')
+                                        .add({
+                                            title : note.title ,
+                                            body : note.body ,
+                                            timestamp : firebase.firestore.FieldValue.serverTimestamp()
+                                        });
+
+        const newID = newFromDB.id; 
+        await setNotes([...notes , note]); 
+        const newNoteIndex = notes.indexOf(notes.filter(_note => _note.id === newID)[0])
+        setSelectedNote(notes[newNoteIndex]);
+        setSelectedNoteIndex(newNoteIndex)
+    } 
+
+    const deleteNote = async (note) => {
+        const noteIndex = notes.indexOf(note);  
+        await setNotes(notes.filter(_note => _note !== note));
+        if(selectedNoteIndex === noteIndex){
+            setSelectedNoteIndex(null);
+            setSelectedNote(null);
+        }else{
+            notes.length > 1 ?
+            selectNote(notes[selectedNoteIndex - 1] , selectedNoteIndex - 1 ) :
+            setSelectedNoteIndex(null);
+            setSelectedNote(null);
+        }
+
+        firebase
+                .firestore()
+                .collection('notes')
+                .doc(note.id)
+                .delete();
+
+    }
+    
+
+    return (
+            <div className = "app-container"> 
+                <SidebarComponent 
+                selectedNoteIndex = {selectedNoteIndex} 
+                notes = {notes}
+                deleteNote = {deleteNote}
+                selectNote = {selectNote}
+                newNote = {newNote}
+                ></SidebarComponent>
+                {
+                    selectedNote ? 
+                    <EditorComponent
+                    selectedNote = {selectedNote}
+                    selectedNoteIndex = {selectedNoteIndex}
+                    notes = {notes}
+                    noteUpdate = {noteUpdate}></EditorComponent>
+                    
+                    : null
+                }
+            </div>
+        )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./App.css"
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyCuPCy3mYwVZZ4RhkpeN-eQfd7_blN2lZw",
+    authDomain: "notes-app-7d2b1.firebaseapp.com",
+    projectId: "notes-app-7d2b1",
+    storageBucket: "notes-app-7d2b1.appspot.com",
+    messagingSenderId: "534691173040",
+    appId: "1:534691173040:web:2f0febe0dab0b6de6b6175"
+  });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
